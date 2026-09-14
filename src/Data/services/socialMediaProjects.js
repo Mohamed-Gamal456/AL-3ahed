4f0f3ab9ccc58cc1aca0a8b1ae22c148cfbed6fd
Mohamed-Gamal456 <m.g.mohamed0123@gmail.com>
2026-09-14 10:57:00 +0200

    Update SEO favicon and social media assets

--- src/Data/services/socialMediaProjects.js
+++ src/Data/services/socialMediaProjects.js
@@ -6,12 +6,12 @@
 // SOCIAL MEDIA PROJECTS IMAGES
 // =====================================================
 
-import socialMedia1 from "../../assets/images/services/social-media/download.jpg";
-import socialMedia2 from "../../assets/images/services/social-media/download.jpg";
-import socialMedia3 from "../../assets/images/services/social-media/download.jpg";
-import socialMedia4 from "../../assets/images/services/social-media/download.jpg";
-import socialMedia5 from "../../assets/images/services/social-media/download.jpg";
-import socialMedia6 from "../../assets/images/services/social-media/download.jpg";
+import socialMedia1 from "../../assets/images/services/social-media/1.jpeg";
+import socialMedia2 from "../../assets/images/services/social-media/2.jpeg";
+import socialMedia3 from "../../assets/images/services/social-media/3.jpeg";
+import socialMedia4 from "../../assets/images/services/social-media/4.jpeg";
+import socialMedia5 from "../../assets/images/services/social-media/5.jpeg";
+import socialMedia6 from "../../assets/images/services/social-media/6.jpeg";
 
 
 // =====================================================
